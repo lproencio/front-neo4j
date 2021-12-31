1e4680d0e216ad1172bfa9b97529b3c70200d5f0
lproencio <leandroproencio@gmail.com>
2021-12-31 13:59:11 -0300

    create nav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/create">Create</router-link>
+    <router-link :class="{ selected: current_route == 'Home' }" to="/"
+      >Home</router-link
+    >
+    <router-link :class="{ selected: current_route == 'New' }" to="/new"
+      >New user</router-link
+    >
   </div>
   <router-view />
 </template>
+
+<script>
+import { ref, watch } from "@vue/runtime-core";
+import { useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const route = useRoute();
+
+    const current_route = ref("");
+
+    watch(
+      () => route.name,
+      () => {
+        current_route.value = route.name;
+      }
+    );
+
+    return { current_route };
+  },
+};
+</script>
